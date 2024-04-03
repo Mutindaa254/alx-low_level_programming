d4bf049c5b639c3033989f56fb46e3b14cd2fb43
Moses Mumo <mosesm5499@gmail.com>
2024-04-03 11:27:57 +0300

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_binary - prints the binary representation
  * of a number.
@@ -7,6 +6,7 @@
  *
  * Return: no return.
  */
+
 void print_binary(unsigned long int n)
 {
 	if (n >> 0)
