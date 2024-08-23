ed6076a27de937b9ba0b3aeda2b40c06b6b6ab4b
Moses Mumo <mosesm5499@gmail.com>
2024-08-23 09:55:28 +0300

    3-alloc_grid.c

--- alx-low_level_programming-master/0x0B-malloc_free/3-alloc_grid.c
+++ alx-low_level_programming-master/0x0B-malloc_free/3-alloc_grid.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-
 /**
  * alloc_grid - Returns a pointer to a 2-dimensional array of integers
  * @width: The width of the 2-dimensional array.
